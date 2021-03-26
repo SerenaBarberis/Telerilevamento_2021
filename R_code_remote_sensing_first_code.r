aeb236307214a5845894ef71675d9ceaab36b576
SerenaBarberis <74198537+SerenaBarberis@users.noreply.github.com>
2021-03-26 10:50:43 +0100

    Update R_code_remote_sensing_first_code.r

--- R_code_remote_sensing_first_code.r
+++ R_code_remote_sensing_first_code.r
@@ -79,6 +79,49 @@ plot(p224r63_2011$B3_sre, col=clr)
 cln <- colorRampPalette(c('red','orange','yellow'))(100)
 plot(p224r63_2011$B4_sre, col=cln)
 
+#come visualizzare i dati con i plot RGB
+#bande di landsat
+#B1: blu
+#B2: verde
+#B3: rosso
+#B4: infrarosso vicino (prima lunghezza d'onda dopo il rosso più vicino al visibile)
+#B5: infrarosso medio
+#B6: termico
+#B7: infrarosso medio
+
+#plotRGB: funzione che plotta un oggetto raster con molti livelli secondo le bande RGB (r3,g2,b1->come vediamo noi l'immagine). stretch:prendiamo la riflettanza delle singole bande per evitare la distorsione verso una sola sfumatura del colore). lin=lineare
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+#Cambiamo la disposizione RGB per visualizzare diversamente l'immagine, R4,G3,B2 (rimuoviamo il blu)
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") #vegetazione rossa
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") #vegetazione verde "fluorescente", si vede bene la componente agricola (viola)
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin") #infrarosso nella componente blu, vegetazione blu, suolo nudo giallo
+
+#montiamo le 4 immagini all'interno dello stesso grafico
+pdf('immagine.pdf') #per salvare l'immagine in pdf
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") 
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+#histogram stretch
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") 
+
+# par colore visibile, falsi colori e falsi colori con histogram stretch
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") #individua tutte le differenze della componente vegetale (non più omogeneo come si vede con lo stretch lineare)
+
+#PCA analisi delle componenti principali 
+install.packages('RStoolbox')
+library(RStoolbox)
+
+
+
 
 
 
