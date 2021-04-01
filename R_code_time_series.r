f9ad33d6d2ef5509098a921bd9f53092472ae25b
SerenaBarberis <74198537+SerenaBarberis@users.noreply.github.com>
2021-04-01 12:35:34 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,6 @@
+#time series analysis: Greenland increase of temperature
+#Data and code from Emanuela Cosma
+
+#install.packages('raster')
+#library(raster)
+setwd('C:/Users/Sery/Desktop/lab/greenland')
