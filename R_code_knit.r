1e876355bb7e9a71d742b9b519121a9b2fd8ff8e
SerenaBarberis <74198537+SerenaBarberis@users.noreply.github.com>
2021-04-16 10:52:58 +0200

    Update R_code_knit.r

--- R_code_knit.r
+++ R_code_knit.r
@@ -2,9 +2,12 @@
 
 setwd('C:/Users/Sery/Desktop/lab')
 library(knitr)
-#knitr: genera un report che andrà a salvare nella stessa cartella del codice 
+#knitr: genera un report che andrà a salvare nella stessa cartella del codice che abbiamo salvato (R_code_greenland.r)
+#funzione stitch: primo argomento è il nome del codice (R_code_greenland.r); template->misc 
 
-
+stitch('R_code_greenland.r.txt', template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#produce un file .tex
+# crea una cartella figure con le singole immagini in formato pdf
 
 
 
